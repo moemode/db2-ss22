af7e296a6de8535ec63a75e01701eaa48bd4ef8c
moemode <pfeffer.moritz@gmail.com>
2023-07-17 21:05:56 +0200

    Copy unary table data out of monet db instance, change mmap.c to allow for path as cmd argument

--- material/Chapter02/mmap.c
+++ material/Chapter02/mmap.c
@@ -1,54 +1,52 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <assert.h>
-#include <stdint.h>
-
-/* path to BAT's tail file */
-#define TAIL "/Users/grust/DB2/course/MonetDB/data/scratch/bat/03/330.tail"
-
 
 /* scan the tail file (assuming it is of type int, column name 'a') */
-void scan_tail(int32_t *tail, off_t size)
-{
-  for (off_t i = 0; i < size / sizeof(int32_t); i += 1) {
-    printf("row #%lld: a = %d\n", i, tail[i]);
-  }
+void scan_tail(int32_t *tail, off_t size) {
+    for (off_t i = 0; i < size / sizeof(int32_t); i += 1) {
+        printf("row #%jd: a = %d\n", i, tail[i]);
+    }
 }
-
 
 /* mmap file at ‹path›, return address and ‹size› of memory map */
-void* mmap_file(char *path, off_t *size)
-{
-  int fd;
-  struct stat status;
-  void *map;
+void *mmap_file(char *path, off_t *size) {
+    int fd;
+    struct stat status;
+    void *map;
 
-  fd = open(path, O_RDONLY);
-  assert(fd >= 0);
-  assert(stat(path, &status) == 0);
-  *size = status.st_size;
+    fd = open(path, O_RDONLY);
+    assert(fd >= 0);
+    assert(stat(path, &status) == 0);
+    *size = status.st_size;
 
-  map = mmap(NULL, *size, PROT_READ, MAP_SHARED, fd, 0);
-  assert(map != MAP_FAILED);
+    map = mmap(NULL, *size, PROT_READ, MAP_SHARED, fd, 0);
+    assert(map != MAP_FAILED);
 
-  close(fd);
+    close(fd);
 
-  return map;
+    return map;
 }
 
+int main(int argc, char *argv[]) {
 
-int main()
-{
-  off_t tail_size;
-  void *tail_map;
+    if (argc < 2) {
+        printf("Usage: %s <tail_file_path>\n", argv[0]);
+        return 1;
+    }
 
-  /* map tail file into memory */
-  tail_map = mmap_file(TAIL, &tail_size);
+    off_t tail_size;
+    void *tail_map;
+    char *tail_path = argv[1];
 
-  scan_tail(tail_map, tail_size);
+    /* map tail file into memory */
+    tail_map = mmap_file(tail_path, &tail_size);
 
-  return 0;
+    scan_tail(tail_map, tail_size);
+
+    return 0;
 }
